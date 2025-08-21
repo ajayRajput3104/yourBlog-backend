88a7dea2866a6cc6858deeb88aef26ba7813c98c
ajayRajput3104 <2004ajrajput@gmail.com>
2025-08-21 23:14:14 +0530

    updated Frontend origin

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import express from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import connectDB from './config/db.js'
-import authRoutes from './routes/authRoutes.js'
-import postRoutes from './routes/postRoutes.js'
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 import fileRoutes from "./routes/fileRoutes.js";
-import errorHandler from './middleware/errorMiddleware.js'
-import env from './config/env.js'
+import errorHandler from "./middleware/errorMiddleware.js";
+import env from "./config/env.js";
 
-const app=express();
+const app = express();
 
 //connect to Mongo DB
 connectDB();
@@ -17,17 +17,19 @@ connectDB();
 //security Middleware
 app.use(helmet());
 //logger middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // parse JSON + form data
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors())
-// app.use(cors({
-//   origin: ['http://localhost:5173',env.frontendOrigin],
-//   credentials: true
-// }));
+//CORS setup (local + production)
+app.use(
+  cors({
+    origin: ["http://localhost:5173", env.frontendOrigin],
+    credentials: true,
+  })
+);
 
 // 🔍 Request logger - add here
 app.use((req, res, next) => {
@@ -36,18 +38,18 @@ app.use((req, res, next) => {
 });
 
 //Routes
-app.use("/api/auth",authRoutes);
-app.use("/api/posts",postRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 app.use("/api/file", fileRoutes);
 
 // Root route for quick testing
-app.get("/",(req,res)=>{
-    res.send("API is running...");
-})
-//Error handling middleware 
-app.use(errorHandler)
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+//Error handling middleware
+app.use(errorHandler);
 
-const PORT=env.port||5000;
-app.listen(PORT,()=>{
-    console.log(`🚀 Server running on http://localhost:${PORT}`)
-})
+const PORT = env.port || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
